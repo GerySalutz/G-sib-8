2c1c3c8b011094816e087c813c964915fb280d2c
GerySalutz <geriislami25@gmail.com>
2023-10-07 21:59:45 +0700

    first commit hw 8

---
+++ db/seeding.sql
@@ -0,0 +1,7 @@
+INSERT INTO actor (actor_id, first_name, last_name, last_update)
+VALUES (202, 'Geri', 'Salutz', NOW()),
+  (203, 'Ikusaba', 'Mukuro', NOW()),
+  (204, 'Koyomi', 'Araragi', NOW()),
+  (205, 'Klein', 'Morreti', NOW()),
+  (206, 'Yomi', 'Novel', NOW()),
+  (207, 'Yami', 'Kuro', NOW());
